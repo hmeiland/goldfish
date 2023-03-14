230427c851eaa6678a0e2be19bbf1fb81913aa89
hugo meiland <hugo.meiland@microsoft.com>
2023-03-14 19:59:20 +0100

    dynamic secret_key

--- webapp/settings.py
+++ webapp/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&5ep7b6^wartf&mfh^)*udcc0e9pdopwuob4l9)ph9r_t2v2c='
+# SECRET_KEY = 'django-insecure-&5ep7b6^wartf&mfh^)*udcc0e9pdopwuob4l9)ph9r_t2v2c='
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
